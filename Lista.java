5503e70acb5565994300fdc4cd76245f85109886
Fernando <fernandopereirasalvador@gmail.com>
2023-02-19 11:49:41 +0100

    Comentarios añadidos

--- Lista.java
+++ Lista.java
@@ -15,11 +15,11 @@ public class Lista {
         int[] v1 = new int[10];
         int[] v2 = new int[10];
         int i, contador=0;
-        
+//Primera lista
         for(i=0;i<v1.length;i++){
             v1[i] = Funcionetes.lligInt("Introduce el numero " + (i+1) + ": ");
         }
-        
+//Segunda lista
         for (i=v1.length-1;i>-1;i--){
             v2[contador] = v1[i];
             contador++;
@@ -27,14 +27,14 @@ public class Lista {
         
         System.out.println("");
         System.out.println("La lista 2 es: ");
-        
+//Tercera lista
         for(i=0;i<v1.length;i++){
             System.out.println(v2[i]);
         }
         
         System.out.println("");
         System.out.println("La sumas es: ");
-        
+//Cuarta lista
         for(i=0;i<v1.length;i++){
             System.out.println("La suma de " + v1[i] + " + " + v2[i] + " = " + (v1[i]+v2[i]));
         }
