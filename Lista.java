6f449f118ba5cc892075023c3930b12945b13ea1
Fernando <fernandopereirasalvador@gmail.com>
2023-02-19 12:26:21 +0100

    Comentario de la nueva lista añadido

--- Lista.java
+++ Lista.java
@@ -38,6 +38,7 @@ public class Lista {
         for(i=0;i<v1.length;i++){
             System.out.println("La suma de " + v1[i] + " + " + v2[i] + " = " + (v1[i]+v2[i]));
         }
+//Quinta lista
 	List<String> ejemploLista = new ArrayList<String>();
     }
 }
